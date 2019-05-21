4f09fdb91f8ecf78bf2707af615093ca7e19acb3
mrbom <mrbomstar@gmail.com>
2019-05-21 21:06:25 +0300

    fix cors 0

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const toursDb = new db();
 
 toursDb.connect();
 
+app.use(cors());
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cors());
 
 app.post('/fetch-tours', function (req, res) {
     const body = req.body;
